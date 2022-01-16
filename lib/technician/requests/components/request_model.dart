d3f86ccaec60c07f6c681833f016258406d6691f
Zagir Selimov <seljmov@list.ru>
2022-01-16 10:13:54 +0400

    Страница подробностей заявки

--- lib/technician/requests/components/request_model.dart
+++ lib/technician/requests/components/request_model.dart
@@ -2,12 +2,20 @@ class Request {
   final int id;
   final String company;
   final String address;
+  final String manager;
+  final bool highPriority;
+  final String creatorName;
+  final String description;
   final String date;
 
   Request({
     required this.id,
     required this.company,
     required this.address,
+    required this.manager,
+    required this.highPriority,
+    required this.creatorName,
+    required this.description,
     required this.date,
   });
 }
---
+++ lib/technician/requests/request_detail_screen.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:kupol_app/technician/requests/components/request_model.dart';
+import 'package:kupol_app/technician/requests/request_technical_details_card.dart';
+
+class RequestDetailScreen extends StatelessWidget {
+  RequestDetailScreen({Key? key, required this.request}) : super(key: key);
+
+  final Request request;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        title: Text("Подробности заявки"),
+        leading: GestureDetector(
+          onTap: () => Navigator.pop(context),
+          child: Icon(
+            Icons.arrow_back_ios_new_rounded,
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: 10,
+          horizontal: 24,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Text(
+              "Заявка #${request.id}",
+              style: TextStyle(
+                fontSize: 22,
+                color: Theme.of(context).textTheme.bodyText1?.color,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            SizedBox(height: 16),
+            Row(
+              children: [
+                SvgPicture.asset(
+                  "lib/assets/icons/clock.svg",
+                ),
+                SizedBox(width: 8),
+                Text(
+                  request.date,
+                  style: TextStyle(
+                    color: Theme.of(context).textTheme.bodyText1?.color,
+                  ),
+                ),
+                SizedBox(width: 8),
+              ],
+            ),
+            SizedBox(height: 16),
+            RequestTechnicalDetailsCard(request: request),
+            SizedBox(height: 16),
+            Text(
+              request.description,
+              style: TextStyle(
+                fontSize: 16,
+                color: Theme.of(context).textTheme.bodyText1?.color,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
